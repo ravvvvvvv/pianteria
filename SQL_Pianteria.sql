58811f83b81dd49037fb64af047e23189e760bca
Leonardo <90787803+ravvvvvvv@users.noreply.github.com>
2023-03-23 08:59:31 +0100

    Update SQL_Pianteria.sql

--- SQL_Pianteria.sql
+++ SQL_Pianteria.sql
@@ -12,25 +12,25 @@ prezzo double not null,
 quantità int not null,
 adottabile int not null,
 prezzo_adozione int not null
-)
+);
 
 create table stagione(
 id int auto_increment primary key,
 nome nvarchar(9) not null
-)
+);
 
 create table pianta_stagione(
 id int auto_increment primary key,
 id_pianta int not null,
 id_stagione int not null
-)
+);
 
 create table fornitore(
 id int auto_increment primary key,
 nome nvarchar(20) not null,
 email nvarchar(30) not null,
 telefono nvarchar(14) not null
-)
+);
 
 create table ordine_fornitore(
 id int auto_increment primary key,
@@ -40,14 +40,14 @@ quantità int not null,
 data_ordine date not null,
 data_arrivo date not null,
 id_user int not null
-)
+);
 
 create table pianta_ordine(
 id int auto_increment primary key,
 id_pianta int not null,
 id_ordine int not null,
 quantità int not null
-)
+);
 
 create table ordine(
 id int auto_increment primary key,
@@ -55,13 +55,13 @@ id_user int not null,
 data_acquisto date not null,
 data_ritiro date not null,
 id_punto_ritiro int not null
-)
+);
 
 create table punto_ritiro(
 id int auto_increment primary key,
 nome nvarchar(30) not null,
 indirizzo nvarchar(100)
-)
+);
 
 create table utente(
 id int auto_increment primary key,
@@ -69,7 +69,7 @@ username nvarchar(30) not null,
 email nvarchar(30) not null,
 `password` nvarchar(30) not null,
 livello_permessi int not null
-)
+);
 
 alter table pianta_stagione
 add foreign key (id_pianta) references pianta(id);
