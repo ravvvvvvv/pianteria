20138c475214485cd501337cd86e2edf9ce6a74d
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-03-31 11:54:02 +0200

    Create login.php

---
+++ backend/API/utente/login.php
@@ -0,0 +1,39 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/../../COMMON/$class.php";
+});
+
+set_exception_handler("errorHandler::handleException");
+set_error_handler("errorHandler::handleError");
+
+require __DIR__ . '/../../MODEL/Utente.php';
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+}
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (empty($data->email) || empty($data->password)) {
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+    die();
+}
+
+$utente = new Utente();
+
+$id = $utente->login($data->email, $data->password);
+
+if ($id === false) {
+    http_response_code(400);
+    echo json_encode(["id" => "-1", "message" => "Nessun utente trovato con le credenziali fornite"]);
+}
+else
+{
+    echo json_encode($id);
+}
