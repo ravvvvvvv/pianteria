0695435f729984e4fb934873c8a553d11d68e1b5
Riccardo-Barchi <90723294+Riccardo-Barchi@users.noreply.github.com>
2023-03-31 13:01:50 +0200

    Add files via upload

---
+++ backend/MODEL/ordine.php
@@ -0,0 +1,27 @@
+<?php
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/../COMMON/$class.php";
+});
+
+set_exception_handler("errorHandler::handleException");
+set_error_handler("errorHandler::handleError");
+
+class Ordine
+{
+    private PDO $conn;
+    private Connect $db;
+
+    public function __construct() //Si connette al DB.
+    {
+        $this->db = new Connect;
+        $this->conn = $this->db->getConnection();
+    }
+
+    public function getArchiveOrder() {
+        $sql = "SELECT * FROM ordine WHERE 1=1";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
