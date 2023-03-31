32f0c0886c6553be7244942acb748e1a9a92501c
Riccardo-Barchi <90723294+Riccardo-Barchi@users.noreply.github.com>
2023-03-31 09:12:25 +0200

    Update pianta.php

--- backend/MODEL/pianta.php
+++ backend/MODEL/pianta.php
@@ -32,10 +32,10 @@ class Pianta
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
-    public function createPianta($nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantità, $adottabile, $prezzo_adozione, $inizio_raccolto, $fine_raccolto){
+    public function createPianta($nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantità, $adottabile, $prezzo_adozione, $inizio_raccolto, $fine_raccolto, $active, $stagione){
         if(empty($inizio_raccolto) && empty($fine_raccolto)){
-            $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, prezzo, quantità, adottabile, prezzo_adozione)
-                VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :prezzo, :quantita, :adottabile, :prezzo_adozione)";
+            $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, prezzo, quantità, adottabile, prezzo_adozione, active)
+                VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :prezzo, :quantita, :adottabile, :prezzo_adozione, :active)";
         }else {
             $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, prezzo, quantità, adottabile, prezzo_adozione, inizio_raccolto, :fine_raccolto)
             VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :prezzo, :quantita, :adottabile, :prezzo_adozione, :inizio_raccolto, :fine_raccolto)";
@@ -52,7 +52,8 @@ class Pianta
         $stmt->bindValue(":quantita",$quantità,PDO::PARAM_INT);
         $stmt->bindValue(":adottabile",$adottabile,PDO::PARAM_INT);
         $stmt->bindValue(":prezzo_adozione",$prezzo_adozione,PDO::PARAM_STR);
-        
+        $stmt->bindValue(":active",$active,PDO::PARAM_INT);
+
         if(!empty($inizio_raccolto)){
             $stmt->bindValue(":inizio_raccolto",$inizio_raccolto,PDO::PARAM_STR);
         }
@@ -61,6 +62,20 @@ class Pianta
             $stmt->bindValue(":fine_raccolto",$fine_raccolto,PDO::PARAM_STR);
         }
 
-        return $stmt->execute();
+        $result = $stmt->execute();
+
+        $last_id = $this->conn->lastInsertId();
+
+        $sql2 = "INSERT INTO pianta_stagione (id_pianta, id_stagione)
+                VALUES (:id_pianta, :id_stagione)";
+        foreach($stagione as $stg){
+            $stmt = $this->conn->prepare($sql2);
+            $stmt->bindValue(":id_pianta",$last_id, PDO::PARAM_INT);
+            $stmt->bindValue(":id_stagione",$stg, PDO::PARAM_INT);
+
+            $stmt->execute();
+        }
+
+        return $result;
     }
 }
