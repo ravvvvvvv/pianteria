0f3e1672510bedfddba35cdd86aeadf4031f2291
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-06-07 19:20:48 +0200

    Update ordine.php

--- backend/MODEL/ordine.php
+++ backend/MODEL/ordine.php
@@ -18,7 +18,13 @@ class Ordine
     }
 
     public function getArchiveOrder() {
-        $sql = "SELECT * FROM ordine WHERE 1=1";
+        $sql = "SELECT ordine.id, utente.username, ordine.data_acquisto, ordine.data_ritiro, (pianta_ordine.quantità * pianta.prezzo) as costo
+				FROM utente
+				INNER JOIN ordine ON ordine.id_user = utente.id 
+				INNER JOIN pianta_ordine ON pianta_ordine.id_ordine = ordine.id 
+                INNER JOIN punto_ritiro ON punto_ritiro.id = ordine.id_punto_ritiro
+				INNER JOIN pianta ON pianta_ordine.id_pianta = pianta.id";
+                
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
 
@@ -26,7 +32,13 @@ class Ordine
     }
 
     public function getOrder($id_ordine) {
-        $sql = "SELECT * FROM ordine WHERE id = :id_ordine";
+        $sql = "SELECT utente.username, punto_ritiro.nome as punto_ritiro, pianta.stato_pianta, ordine.data_acquisto, ordine.data_ritiro, punto_ritiro.indirizzo, pianta.nome, pianta_ordine.quantità as quantità, (pianta_ordine.quantità * pianta.prezzo) as costo
+        FROM ordine 
+        INNER JOIN utente on utente.id = ordine.id_user
+        INNER JOIN punto_ritiro on punto_ritiro.id = ordine.id_punto_ritiro
+        INNER JOIN pianta_ordine on pianta_ordine.id_ordine = ordine.id
+        inner join pianta on pianta.id = pianta_ordine.id_pianta
+        WHERE ordine.id = :id_ordine";
 
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(":id_ordine", $id_ordine,PDO::PARAM_INT);  
