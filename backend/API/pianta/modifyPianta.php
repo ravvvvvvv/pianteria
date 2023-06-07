61d83e51b2db0f8a627cb748045a8860c8329ce1
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-06-07 19:27:15 +0200

    Update modifyPianta.php

--- backend/API/pianta/modifyPianta.php
+++ backend/API/pianta/modifyPianta.php
@@ -5,31 +5,21 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+}
+
 $data = json_decode(file_get_contents("php://input"));
 
-if(empty($data->id) || empty($data->nome) || empty($data->nome_scientifico) || empty($data->fiore) || empty($data->colore1) || empty($data->colore2) || empty($data->prezzo) || empty($data->quantità) || empty($data->adottabile) || empty($data->prezzo_adozione) || empty($data->stato_pianta) || empty($data->stagione)){
+if(empty($data->id) || empty($data->nome) || empty($data->nome_scientifico) || empty($data->fiore) || empty($data->colore1) || empty($data->colore2) || empty($data->prezzo) || empty($data->quantità) || empty($data->adottabile) || empty($data->prezzo_adozione) || empty($data->inizio_raccolto) || empty($data->fine_raccolto) || empty($data->stato_pianta) || empty($data->stagione)){
     http_response_code(404);
     echo json_encode(["message" => "Insert valid input"]);
     exit();
 }
 
-$inizio_raccolto;
-if(empty($data->inizio_raccolto)){
-    $inizio_raccolto = null;
-}else{
-    $inizio_raccolto = $data->inizio_raccolto;
-}
-
-$fine_raccolto;
-if(empty($data->fine_raccolto)){
-    $fine_raccolto = null;
-}else{
-    $fine_raccolto = $data->fine_raccolto;
-}
-
 $pianta = new Pianta();
 
-$result = $pianta->modifyPianta($data->id, $data->nome, $data->nome_scientifico, $data->fiore, $data->colore1, $data->colore2, $data->prezzo, $data->quantità, $data->adottabile, $data->prezzo_adozione, $inizio_raccolto, $fine_raccolto, $data->stato_pianta, $data->stagione);
+$result = $pianta->modifyPianta($data->id, $data->nome, $data->nome_scientifico, $data->fiore, $data->colore1, $data->colore2, $data->prezzo, $data->quantità, $data->adottabile, $data->prezzo_adozione, $data->inizio_raccolto, $data->fine_raccolto, $data->stato_pianta, $data->stagione);
 
 if ($result) {
     http_response_code(404);
