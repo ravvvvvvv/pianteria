6e84865c1886029c4dc2f742fb535cfe159a76e9
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-06-07 19:29:12 +0200

    Update getUtente.php

--- backend/API/utente/getUtente.php
+++ backend/API/utente/getUtente.php
@@ -1,10 +1,13 @@
 <?php
 require __DIR__ . '/../../MODEL/Utente.php';
 header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[6])) {
+if (empty($parts[5])) {
     http_response_code(404);
     echo json_encode(["message" => "Insert a valid ID"]);
     exit();
@@ -12,7 +15,7 @@ if (empty($parts[6])) {
 
 $utente = new Utente();
 
-$result = $utente->getUtente($parts[6]);
+$result = $utente->getUtente($parts[5]);
 
 if ($result != false) {
     echo json_encode($result);
