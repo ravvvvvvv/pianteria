21f264f212bdf9e0a734888a9a7af97f3fac150e
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-06-07 19:21:16 +0200

    Update pianta.php

--- backend/MODEL/pianta.php
+++ backend/MODEL/pianta.php
@@ -18,28 +18,49 @@ class Pianta
     }
 
     public function getArchivePianta(){
-        $sql = "SELECT * FROM pianta p WHERE 1=1";
+        $sql = "SELECT p.id, p.nome, p.nome_scientifico, s.nome as stagione, p.prezzo, p.fiore, p.stato_pianta
+        		FROM pianta p
+                INNER JOIN stagione s on s.id = p.id_stagione
+                WHERE 1=1";
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getPianta($id_pianta) {
-        $sql = "SELECT * FROM pianta p WHERE p.id = :id_pianta";
+        $sql = "SELECT p.*, s.nome as stagione
+        		FROM pianta p
+                INNER JOIN stagione s on s.id = p.id_stagione
+                WHERE p.id = :id_pianta";
         $stmt = $this->conn->prepare($sql);
         $stmt->BindValue(":id_pianta",$id_pianta,PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
-    public function createPianta($nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantità, $adottabile, $prezzo_adozione, $inizio_raccolto, $fine_raccolto, $stato_pianta, $stagione){
-        if(empty($inizio_raccolto) && empty($fine_raccolto)){
-            $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, prezzo, quantità, adottabile, prezzo_adozione, active)
-                VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :prezzo, :quantita, :adottabile, :prezzo_adozione, :active)";
-        }else {
-            $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, prezzo, quantità, adottabile, prezzo_adozione, inizio_raccolto, :fine_raccolto)
-            VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :prezzo, :quantita, :adottabile, :prezzo_adozione, :inizio_raccolto, :fine_raccolto)";
-        }
+    public function createPianta($nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantità, $adottabile, $stato_pianta, $stagione){
+      $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, id_stagione, prezzo, quantità, adottabile, stato_pianta)
+      			VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :id_stagione, :prezzo, :quantita, :adottabile, :stato_pianta)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":nome",$nome,PDO::PARAM_STR);
+        $stmt->bindValue(":nome_scientifico",$nome_scientifico,PDO::PARAM_STR);
+        $stmt->bindValue(":fiore",$fiore,PDO::PARAM_INT);
+        $stmt->bindValue(":colore1",$colore1,PDO::PARAM_STR);
+        $stmt->bindValue(":colore2",$colore2,PDO::PARAM_STR);
+        $stmt->bindValue(":id_stagione",$stagione,PDO::PARAM_INT);
+        $stmt->bindValue(":prezzo",$prezzo,PDO::PARAM_STR);
+        $stmt->bindValue(":quantita",$quantità,PDO::PARAM_INT);
+        $stmt->bindValue(":adottabile",$adottabile,PDO::PARAM_INT);
+        $stmt->bindValue(":stato_pianta",$stato_pianta,PDO::PARAM_INT);
+        
+        return $stmt->execute();;
+    }
+
+   	public function createPiantaAdottabile($nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantità, $adottabile, $prezzo_adozione, $inizio_raccolto, $fine_raccolto, $stato_pianta, $stagione){
+        $sql = "INSERT INTO pianta (nome, nome_scientifico, fiore, colore1, colore2, id_stagione, prezzo, quantità, adottabile, prezzo_adozione, stato_pianta, inizio_raccolto, fine_raccolto)
+              	VALUES (:nome, :nome_scientifico, :fiore, :colore1, :colore2, :id_stagione, :prezzo, :quantita, :adottabile, :prezzo_adozione, :stato_pianta, :inizio_raccolto, :fine_raccolto)";
         
         $stmt = $this->conn->prepare($sql);
 
@@ -48,54 +69,28 @@ class Pianta
         $stmt->bindValue(":fiore",$fiore,PDO::PARAM_INT);
         $stmt->bindValue(":colore1",$colore1,PDO::PARAM_STR);
         $stmt->bindValue(":colore2",$colore2,PDO::PARAM_STR);
+        $stmt->bindValue(":id_stagione",$stagione,PDO::PARAM_INT);
         $stmt->bindValue(":prezzo",$prezzo,PDO::PARAM_STR);
         $stmt->bindValue(":quantita",$quantità,PDO::PARAM_INT);
         $stmt->bindValue(":adottabile",$adottabile,PDO::PARAM_INT);
         $stmt->bindValue(":prezzo_adozione",$prezzo_adozione,PDO::PARAM_STR);
-        $stmt->bindValue(":active",$stato_pianta,PDO::PARAM_INT);
-
-        if(!empty($inizio_raccolto)){
-            $stmt->bindValue(":inizio_raccolto",$inizio_raccolto,PDO::PARAM_STR);
-        }
-        if(!empty($fine_raccolto)){
-            echo "COME";
-            $stmt->bindValue(":fine_raccolto",$fine_raccolto,PDO::PARAM_STR);
-        }
-
-        $result = $stmt->execute();
-
-        $last_id = $this->conn->lastInsertId();
-
-        $sql2 = "INSERT INTO pianta_stagione (id_pianta, id_stagione)
-                VALUES (:id_pianta, :id_stagione)";
-        foreach($stagione as $stg){
-            $stmt = $this->conn->prepare($sql2);
-            $stmt->bindValue(":id_pianta",$last_id, PDO::PARAM_INT);
-            $stmt->bindValue(":id_stagione",$stg, PDO::PARAM_INT);
-
-            $stmt->execute();
-        }
-
-        return $result;
+        $stmt->bindValue(":id_stagione",$stagione,PDO::PARAM_INT);
+        $stmt->bindValue(":stato_pianta",$stato_pianta,PDO::PARAM_INT);
+      	$stmt->bindValue(":inizio_raccolto",$inizio_raccolto,PDO::PARAM_STR);
+      	$stmt->bindValue(":fine_raccolto",$fine_raccolto,PDO::PARAM_STR);
+        
+        return $stmt->execute();;
     }
 
-    public function modifyPianta($id_pianta, $nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantita, $adottabile, $prezzo_adozione, $inizio_raccolto, $fine_raccolto, $stato_pianta, $stagione){
-        if(empty($inizio_raccolto) && empty($fine_raccolto)){
-            $sql = "UPDATE pianta 
-            set nome = :nome, nome_scientifico = :nome_scientifico, fiore = :fiore, colore1 = :colore1, 
-            colore2 = :colore2, prezzo = :prezzo, quantità = :quantita, adottabile = :adottabile, 
-            prezzo_adozione = :prezzo_adozione, stato_pianta = :stato_pianta
-            where id = :id_pianta";
-        }else {
-            $sql = "UPDATE pianta 
-            set nome = :nome, nome_scientifico = :nome_scientifico, fiore = :fiore, colore1 = :colore1, 
+    public function modifyPianta($id_pianta, $nome, $nome_scientifico, $fiore, $colore1, $colore2, $prezzo, $quantita, $adottabile,  $prezzo_adozione, $inizio_raccolto, $fine_raccolto, $stato_pianta, $stagione) {
+      	$sql = "UPDATE pianta 
+        	set nome = :nome, nome_scientifico = :nome_scientifico, fiore = :fiore, colore1 = :colore1, 
             colore2 = :colore2, prezzo = :prezzo, quantità = :quantita, adottabile = :adottabile, 
             prezzo_adozione = :prezzo_adozione, inizio_raccolto = :inizio_raccolto, fine_raccolto = :fine_raccolto,
-            stato_pianta = :stato_pianta
+            stato_pianta = :stato_pianta, id_stagione = :stagione
             where id = :id_pianta";
-        }
-        
-        $stmt = $this->conn->prepare($sql);
+            
+     	$stmt = $this->conn->prepare($sql);
 
         $stmt->bindValue("id_pianta",$id_pianta,PDO::PARAM_INT);
         $stmt->bindValue(":nome",$nome,PDO::PARAM_STR);
@@ -108,37 +103,14 @@ class Pianta
         $stmt->bindValue(":adottabile",$adottabile,PDO::PARAM_INT);
         $stmt->bindValue(":prezzo_adozione",$prezzo_adozione,PDO::PARAM_STR);
         $stmt->bindValue(":stato_pianta",$stato_pianta,PDO::PARAM_INT);
-
-        if(!empty($inizio_raccolto)){
-            $stmt->bindValue(":inizio_raccolto",$inizio_raccolto,PDO::PARAM_STR);
-        }
-        if(!empty($fine_raccolto)){
-            $stmt->bindValue(":fine_raccolto",$fine_raccolto,PDO::PARAM_STR);
-        }
-
-        $result = $stmt->execute();
-
-        //Cancello tutte le stagioni precendenti
-        $sql2 = "DELETE FROM pianta_stagione
-                WHERE id_pianta = :id";
-        $stmt = $this->conn->prepare($sql2);
-        $stmt->bindValue(":id",$id_pianta,PDO::PARAM_INT);
-        $stmt->execute();
-
-        //Inseriamo le nuove stagioni
-        $sql3 = "INSERT INTO pianta_stagione(id_pianta, id_stagione)
-                VALUES(:id_pianta, :id_stagione)";
-        foreach($stagione as $stg){
-            $stmt = $this->conn->prepare($sql3);
-            $stmt->bindValue(":id_pianta",$id_pianta, PDO::PARAM_INT);
-            $stmt->bindValue(":id_stagione",$stg, PDO::PARAM_INT);
-
-            $stmt->execute();
-        }
-
-        return $result;
+      	$stmt->bindValue(":inizio_raccolto",$inizio_raccolto,PDO::PARAM_STR);
+		$stmt->bindValue(":fine_raccolto",$fine_raccolto,PDO::PARAM_STR);
+        $stmt->bindValue(":stato_pianta",$stato_pianta,PDO::PARAM_INT);
+        $stmt->bindValue(":stagione",$stagione,PDO::PARAM_INT);
+        
+      	return $stmt->execute();
     }
-
+    
     public function modifyActivePianta($id_pianta, $active){
         $sql = "UPDATE pianta p
         SET stato_pianta = :active
