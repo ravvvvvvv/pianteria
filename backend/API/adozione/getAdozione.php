55495854ea8d3c2f27b9bb2c1ef63b7fc58c0028
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-06-07 19:23:58 +0200

    Update getAdozione.php

--- backend/API/adozione/getAdozione.php
+++ backend/API/adozione/getAdozione.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
-if(empty($parts[6])){
+if(empty($parts[5])){
     http_response_code(404);
     echo json_encode(["message" => "Insert a valid ID"]);
     exit();
@@ -14,11 +14,11 @@ if(empty($parts[6])){
 
 $adozione = new Adozione();
 
-$result = $adozione->getAdozione($parts[6]);
+$result = $adozione->getAdozione($parts[5]);
 
-if ($result != false) {
+if ($result) {
     echo json_encode($result);
 } else {
     http_response_code(400);
-    echo json_encode(["message" => "Fornitore not found"]);
+    echo json_encode(["message" => "Adozione not found"]);
 }
